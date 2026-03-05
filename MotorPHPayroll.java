9789ab219cf8f9195fe7fbcba258a92e7732c281
jejomirana-byte <jejomirana@gmail.com>
2026-03-05 16:06:04 +0800

    Added method skeleton for payroll system

---
+++ MotorPHPayroll.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class MotorPHPayroll {
+
+    public static void main(String[] args) {
+
+        login();
+
+    }
+
+    // LOGIN SYSTEM
+    static void login() {
+
+    }
+
+    // EMPLOYEE MENU
+    static void employeeMenu() {
+
+    }
+
+    // PAYROLL STAFF MENU
+    static void payrollMenu() {
+
+    }
+
+    // EMPLOYEE DATA MODULE
+    static void readEmployeeData() {
+
+    }
+
+    // ATTENDANCE + GRACE PERIOD
+    static double computeDailyHours(String login, String logout) {
+        return 0;
+    }
+
+    // CUTOFF HOURS AGGREGATION
+    static double computeCutoffHours(int employeeNumber) {
+        return 0;
+    }
+
+    // SALARY + DEDUCTIONS
+    static void computeSalary(double hours, double rate) {
+
+    }
+
+    // OUTPUT DISPLAY
+    static void printPayroll() {
+
+    }
+
+}
