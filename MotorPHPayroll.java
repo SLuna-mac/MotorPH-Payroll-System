3d03835cda9168fd7bf9922c804eab03b6eda926
jeremyamado7 <lr.jamado@mmdc.mcl.edu.ph>
2026-03-12 12:38:34 +0800

    Update MotorPHPayroll.java

--- MotorPHPayroll.java
+++ MotorPHPayroll.java
@@ -1,5 +1,9 @@
 import java.util.*;
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class MotorPHPayroll {
 
