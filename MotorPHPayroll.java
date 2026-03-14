c6fa137e35409122f45f21b9cafffdbc49d34280
SLuna-mac <lr.jjmirana@mmdc.mcl.edu.ph>
2026-03-14 16:08:52 +0800

    Update MotorPHPayroll.java

--- MotorPHPayroll.java
+++ MotorPHPayroll.java
@@ -1,9 +1,5 @@
 import java.util.*;
 import java.io.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
 
 public class MotorPHPayroll {
 
